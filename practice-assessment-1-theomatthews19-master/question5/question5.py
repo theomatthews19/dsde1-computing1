0e2d5655b79abdf4104ab7ba83d1bac8238e8d7c
theomatthews01 <theo.matthews19@imperial.ac.uk>
2019-10-29 12:00:33 +0000

    question 5

--- practice-assessment-1-theomatthews19-master/question5/question5.py
+++ practice-assessment-1-theomatthews19-master/question5/question5.py
@@ -31,6 +31,11 @@ def unique_values(input_list):
 # Write a function that returns a string where first "x" characters are lower
 # case. "x" is passed as the second parameter. 
 def start_to_lower(string, x):
+    lower = string[0:x]
+    banana = string[x::]
+    product = lower.lower() + banana
+    return product
+    
 
 
 # Write a function that returns the character which first occurs for the second
@@ -38,19 +43,33 @@ def start_to_lower(string, x):
 # occurs twice first. If no character occurs more than once, an empty string
 # should be returned. 
 def first_double_char(string):
-    return 
+    chars = []
+    for i in string:
+        if i not in chars:
+            chars.append(i)
+        else:
+            return str(i)
+    return""
 
 # Dictionaries
 
 # Write function that returns a sum of all values stored in the dictionary.
 def sum_of_dict(dictionary):
-    return
+    sum = None
+    for y in dictionary:
+        sum = dictionary[y]
+    
+    return sum
 
 # Write a function that returns a new dictionary where each value is multiplied
 # by 3. Example: for input dictionary {'a': 1, 'b': 2, 'c': 3} the returned
 # dictionary will be {'a': 3, 'b': 6, 'c': 9}
 def multiply_dict(dictionary):
-    return
+    new_dictionary = dictionary.copy()
+    for x in new_dictionary:
+        new_dictionary[x] = new_dictionary[x] * 3
+    
+    return new_dictionary
 
 # Arguments
 
@@ -61,6 +80,12 @@ def multiply_dict(dictionary):
 # minimum value from the list. The default operation is "max". If the operation
 # is neither "max" nor "min" the function should return "None" (the value, not
 # the string).
-def max_min(input_list, operation):
-    return
+def max_min(input_list, operation = "max"):
+    maxi_mini = str[max_min]
+    if maxi_mini = "max":
+        return "max"
+    else:
+        return "none"
+    
+    return""
 
