c44ddb2fecf5c43ffed9b9590a652b923bec9961
theomatthews01 <theo.matthews19@imperial.ac.uk>
2019-11-11 17:13:53 +0000

    pylint

--- practice-assessment-1-theomatthews19-master/question1/question1.py
+++ practice-assessment-1-theomatthews19-master/question1/question1.py
@@ -1,9 +1,3 @@
-'''
-question1.py
-
-Implementation of the flowchart in question1.png.
-'''
-
 def flowchart(input_value):
     if input_value == 0:
         return 0
@@ -12,8 +6,6 @@ def flowchart(input_value):
             return "positive-even"
         else:
             return "positive-odd"
-            
-"""asks if is divisible by 2"""
     elif input_value % 2 < 0:
         if input_value % 2 == 0:
             return "negative-even"
