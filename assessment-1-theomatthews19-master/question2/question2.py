125c397bbf6a08c1decd94bffda4dbbbbd44b3c7
theomatthews01 <theo.matthews19@imperial.ac.uk>
2019-11-12 11:11:44 +0000

    Most of 6

--- assessment-1-theomatthews19-master/question2/question2.py
+++ assessment-1-theomatthews19-master/question2/question2.py
@@ -35,20 +35,21 @@ difficult to understand
 '''
 
 def flowchart(input_value):
-    if input_value < 60:
-        if input_value % 2 == 0:
+    """user inputs value for code to check"""
+    if input_value < 60:    # if input_value is less than 60
+        if input_value % 2 == 0:    # checks if input_value is divisible by 2
             return "small-even"
         else:
             return "small-odd"
     
-    elif input_value > 60 and input_value < 100:
-        if input_value % 2 == 0:
+    elif input_value > 60 and input_value < 100:    # if input_value is more than 60 and less than 100
+        if input_value % 2 == 0:    # checks if input_value is divisible by 2
             return "medium-even"
         else:
             return "medium-odd"
     
-    elif input_value > 100:
-        if input_value % 2 == 0:
+    elif input_value > 100:     # if input_value is more than 100
+        if input_value % 2 == 0:    # checks if input_value is divisible by 2
             return "large-even"
         else:
             return "large-odd"
--- assessment-1-theomatthews19-master/question6/question6.py
+++ assessment-1-theomatthews19-master/question6/question6.py
@@ -9,12 +9,20 @@ Simple functions performing operations on basic Python data structures.
 # Write a function that checks if all dictionaries in a list are empty or not.
 # return True if all dictionaries are empty, False otherwise
 def list_of_dicts_empty(input_list):
+    all_empty = True
+    for i in input_list:
+        if i:
+            all_empty = False
 
 # Write a function that returns a list containig all but the first and the last
 # element of "the_list". 
 # example: the_list = [1,2,3,4,5,6]
 # returns [2,3,4,5]
 def all_but_first_and_last(input_list):
+    del all_but_first_and_last[0]
+    del all_but_first_and_last[-1]
+    return all_but_first_and_last
+
 
 
 # Strings
@@ -26,19 +34,37 @@ def all_but_first_and_last(input_list):
 # parameters should be ignored and not include in the length of the sentence (when
 # determining which sentence should go first).
 def merge_sentences(sentence1, sentence2):
+    if len(sentence1) < len(sentence2):
+        merge_sentences.join((sentence1, sentence2))
+    else:
+        merge_sentences.join((sentence2, sentence1))
+
+    return merge_sentences
 
 # Write a function that returns the string in upper case if there are at least
 # 3 uppercase letters in the first 5 letters of the string. If there are not 
 # at least 3 uppercase letters in the first 5 letters of the string, return the
 # original string.
 def upper_case(string):
-
+    number_of_upper = 0
+    for char in string[:5]:
+        if char.upper == char:
+            number_of_upper += 1
+    if number_of_upper >= 3:
+        return string.upper()
+    else:
+        return string.lower()
 
 # Dictionaries
 
 # Write a function that checks whether there is a key in the dictionary whose
 # length is > 3. Function returns True or False.
 def key_longer(dictionary):
+    for i in dictionary():
+        if len(dictionary.key(i)) > 3:
+            return True
+        else:
+            return False
 
 # Write function that checks whether there is a value in the dictionary greater
 # than 5. Function returns True or False
